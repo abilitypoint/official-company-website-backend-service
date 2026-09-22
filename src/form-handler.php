fe9b2ac9334438301b1757f70815c7e907c42d61
phinehas <phinehasenakireru@gmail.com>
2026-09-22 14:22:33 +0100

    fix: update CORS origin to match the official website

--- src/form-handler.php
+++ src/form-handler.php
@@ -1,4 +1,4 @@
-<?php header('Access-Control-Allow-Origin: https://official-company-website.vercel.app');
+<?php header('Access-Control-Allow-Origin: https://www.abilitypoint.com.ng');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
